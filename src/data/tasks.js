56c51bda9f0b34f88c171eed4642392fd311dd32
JuanCamiloGrisales <grisalesariasjuancamilo@gmail.com>
2023-07-01 13:29:52 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,24 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Task 1',
+        description: 'my first task',
+    },
+    {
+        id: 1,
+        title: 'Task 2',
+        description: 'my second task',
+    },
+    {
+        id: 2,
+        title: 'Task 3',
+        description: 'my third task',
+    },
+    {
+        id: 3,
+        title: 'Task 4',
+        description: 'my fourth task',
+    },
+
+
+]
